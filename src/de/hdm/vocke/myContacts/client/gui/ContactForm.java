31d6e2974825b94bd75eca59171b8f121df386f9
theresa-vocke <theresa.vocke@outlook.de>
2018-05-29 16:08:50 +0200

    ContactForm Widgets Grid anlegen

--- src/de/hdm/vocke/myContacts/client/gui/ContactForm.java
+++ src/de/hdm/vocke/myContacts/client/gui/ContactForm.java
@@ -3,10 +3,16 @@ package de.hdm.vocke.myContacts.client.gui;
 import com.google.gwt.cell.client.AbstractCell;
 import com.google.gwt.safehtml.shared.SafeHtmlBuilder;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.datepicker.client.DateBox;
 
+import de.hdm.thies.bankProjekt.client.gui.CustomerForm.ChangeClickHandler;
+import de.hdm.thies.bankProjekt.client.gui.CustomerForm.DeleteClickHandler;
+import de.hdm.thies.bankProjekt.client.gui.CustomerForm.NewClickHandler;
 import de.hdm.vocke.myContacts.shared.bo.Contact;
 
 public class ContactForm extends VerticalPanel {
@@ -20,25 +26,40 @@ public class ContactForm extends VerticalPanel {
 	public ContactForm (Navigation navigation) {
 		this.navigation=navigation;
 	}
+	/**
+	 * Anlegen der GUI Elemente 
+	 */
 	
-	  TextBox firstNameBox;
-	  TextBox lastNameBox;  
-	  TextBox phoneNumberBox;
-	  TextBox streetBox;
-	  TextBox numberBox;
-	  TextBox cityBox;
-	  DateBox birthdayBox;
-	  
-	  Button createButton;
-	  Button updateButton;
-	  Button addButton; 
-	  
-	  VerticalPanel navPanel = new VerticalPanel();
-
-	@Override
-	public void render(Context context, Contact value, SafeHtmlBuilder sb) {
-		// TODO Auto-generated method stub
+	private VerticalPanel vpanel = new VerticalPanel();
+	
+	public void onLoad() {
+		super.onLoad();
 		
-	}
+		Grid contactGrid = new Grid(9, 2);
+		this.add(cotactGrid);
+
+
+		deleteButton.addClickHandler(new DeleteClickHandler());
+		deleteButton.setEnabled(false);
+		customerGrid.setWidget(0, 1, deleteButton);
+		
+		newButton.addClickHandler(new NewClickHandler());
+		newButton.setEnabled(false);
+		customerGrid.setWidget(1, 1, newButton);
+
+		Label firstNameLabel = new Label("Vorname");
+		customerGrid.setWidget(2, 0, firstNameLabel);
+		customerGrid.setWidget(2, 1, firstNameTextBox);
+
+		Label lastNameLabel = new Label("Nachname");
+		customerGrid.setWidget(3, 0, lastNameLabel);
+		customerGrid.setWidget(3, 1, lastNameTextBox);
+
+		HorizontalPanel customerButtonsPanel = new HorizontalPanel();
+		customerGrid.setWidget(4, 1, customerButtonsPanel);
+
+		Button changeButton = new Button("Name ändern");
+		changeButton.addClickHandler(new ChangeClickHandler());
+		customerButtonsPanel.add(changeButton);
 	  
 }
